b2b7ea52d6a4378d44b91f71aff4b174b6a31b7a
julissamackeydev <julissamackeydev@gmail.com>
2020-02-04 07:00:44 -0500

    feat: baseball, dog, abuela drawings

--- src/app/drawings/drawings.component.ts
+++ src/app/drawings/drawings.component.ts
@@ -11,6 +11,24 @@ export class DrawingsComponent implements OnInit {
   selectedImage = ""
   images = [
     {
+      "piece": "/assets/drawings/baseball.jpg",
+      "description": "soft pastel on paper"
+    }
+  ]
+  images2 = [
+    {
+      "piece": "/assets/drawings/dog.jpg",
+      "description": "soft pastel on paper"
+    },
+    {
+      "piece": "/assets/drawings/abuela.jpg",
+      "description": "soft pastel on paper"
+    },{
+      "piece":
+        "/assets/drawings/fountain.jpg",
+      "description": "mechanical pencil on paper"
+    },
+    {
       "piece":
         "/assets/drawings/splayedout.jpg",
       "description": "charcoal on paper"
@@ -22,17 +40,13 @@ export class DrawingsComponent implements OnInit {
       "piece":
         "/assets/drawings/curledup.jpg",
       "description": "charcoal on paper"
-    },{
-      "piece":
-        "/assets/drawings/fountain.jpg",
-      "description": "mechanical pencil on paper"
-    }]
-  images2 = [
-    {
-      "piece":
-        "/assets/drawings/fountain.jpg",
-      "description": "mechanical pencil on paper"
-    }]
+    }];
+  // images3 = [
+  //   {
+  //     "piece":
+  //       "/assets/drawings/fountain.jpg",
+  //     "description": "mechanical pencil on paper"
+  //   }];
   ngOnInit() {
   }
 
