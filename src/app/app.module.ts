ecf3147d4ab3f8a4fcbb84c52481cf639121c6be
julissamackey <julissamackey@gmail.com>
2020-09-26 09:34:54 -0400

    fix: remove unreferenced works sections from app mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
-import { Works1Component } from './works1/works1.component';
 import { Landing2Component } from './landing2/landing2.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { Works2Component } from './works2/works2.component';
 import { Works3Component } from './works3/works3.component';
 import { DrawingsComponent } from './drawings/drawings.component';
 import { CommissionsComponent } from './commissions/commissions.component';
@@ -20,11 +18,9 @@ import { ContemporaryComponent } from './contemporary/contemporary.component';
   declarations: [
     AppComponent,
     LandingComponent,
-    Works1Component,
     Landing2Component,
     AboutComponent,
     ContactComponent,
-    Works2Component,
     Works3Component,
     DrawingsComponent,
     CommissionsComponent,
